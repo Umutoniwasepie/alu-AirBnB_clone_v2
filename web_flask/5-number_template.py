0c36333334cfa251c7d16ecd4ca378ff3c8f1451
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2023-10-19 22:00:58 +0200

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,12 +37,12 @@ def num_route(n):
     return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>')
-def number_template(n):
-    """Retrieve template for request
-    """
-    path = '5-number.html'
-    return render_template(path, n=n)
+@app.route('/number_template/<n>', strict_slashes=False)
+def num_route_template(n):
+    if n.isdigit():
+        return render_template('5-number.html', n=n)
+    else:
+        return "Not a valid number."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
